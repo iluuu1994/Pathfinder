c8ab7dbe65eb15472ab178d781dd9d8daf06b424
Ilija Tovilo <ilija.tovilo@me.com>
2014-08-15 03:13:16 +0200

    Initial Commit

---
+++ Pathfinder/Pathfinder.h
@@ -0,0 +1,19 @@
+//
+//  Pathfinder.h
+//  Pathfinder
+//
+//  Created by Ilija Tovilo on 15/08/14.
+//  Copyright (c) 2014 Ilija Tovilo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pathfinder.
+FOUNDATION_EXPORT double PathfinderVersionNumber;
+
+//! Project version string for Pathfinder.
+FOUNDATION_EXPORT const unsigned char PathfinderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pathfinder/PublicHeader.h>
+
+
